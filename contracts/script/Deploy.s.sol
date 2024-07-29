2a87080624d5f2824df6e80d8807d8895a33644c
0xGWHY <gwanghyunjeon@gmail.com>
2024-07-29 19:59:10 +0900

    done

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -2,16 +2,20 @@
 pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
+import "../src/LD_ProgramFactory.sol";
+import "../src/programs/LD_EduProgram.sol";
 
 contract Deploy is Script {
+    // test coverage bypassing
     function test() public {}
-    function setUp() public {
-        // 스크립트 초기 셋업
-    }
 
     function run() public {
-        vm.broadcast();
-
+        vm.startBroadcast(0x05c190619e57f47a402dab4386a8ee87de0421e8e43dd1caed522bf44d6d4121);
+        LD_EduProgram eduProgram = new LD_EduProgram();
+        new LD_ProgramFactory(
+            address(eduProgram), 50000000000000000, payable(address(0x1acDF5aa05372de83EEA17a2df300A0f1731317B))
+        );
+        vm.stopBroadcast();
         // do something...
     }
 }
